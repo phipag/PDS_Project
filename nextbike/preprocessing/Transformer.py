4bc6453f3e0c30de00580b314d0dd4c3748bb1d5
Philipp Page <github@philipp-page.de>
2020-05-07 15:06:01 +0200

    Fixed a bug and added columns as suggest by https://github.com/phipag/PDS_Project/pull/2#discussion_r419921909.

--- nextbike/preprocessing/Transformer.py
+++ nextbike/preprocessing/Transformer.py
@@ -44,17 +44,20 @@ class Transformer(AbstractValidator):
         :return: None
         """
         # Split the original data into a start and end trips data frame
-        start_gdf = self.__preprocessor.gdf[self.__preprocessor.gdf['trip'] == 'start']
-        end_gdf = self.__preprocessor.gdf[self.__preprocessor.gdf['trip'] == 'end']
+        start_gdf = self.__preprocessor.gdf[self.__preprocessor.gdf['trip'] == 'start'].reset_index(drop=True)
+        end_gdf = self.__preprocessor.gdf[self.__preprocessor.gdf['trip'] == 'end'].reset_index(drop=True)
         # Initialize a new GeoDataFrame and calculate the target columns with native pandas
         self.__gdf = gpd.GeoDataFrame(crs='EPSG:4326')
         self.__gdf['bike_number'] = start_gdf['b_number']
         self.__gdf['start_time'] = start_gdf['datetime']
         self.__gdf['weekend'] = start_gdf['datetime'].dt.dayofweek // 5 == 1
         self.__gdf['start_position'] = start_gdf['geometry']
-        self.__gdf['duration'] = end_gdf['datetime'] - start_gdf['datetime']
+        self.__gdf['start_position_name'] = start_gdf['p_name']
+        self.__gdf['duration'] = (end_gdf['datetime'] - start_gdf['datetime']).dt.seconds
         self.__gdf['end_time'] = end_gdf['datetime']
         self.__gdf['end_position'] = end_gdf['geometry']
+        self.__gdf['end_position_name'] = end_gdf['p_name']
+        self.__gdf['is_station'] = (start_gdf['p_place_type'] == 12) & (end_gdf['p_place_type'] == 12)
         if validate:
             self.validate()
 
