39f7e495d0a6d72431dfef43ce5efcb938575f03
phipag <github@philipp-page.de>
2020-06-08 20:07:06 +0200

    Fill in proper values for setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='PDS_Project',
-    version='0.0.1dev1',
-    description='Semester Project - Programming Data Science',
-    author='Student',
-    author_email='student@uni-koeln.de',
+    version='1.0',
+    description='Semester Project - Programming in Data Science',
+    author='Lukas Humpe, Tim Schaefer, Michael The-Lan Bui, Philipp Page',
+    author_email='lhumpe@smail.uni-koeln.de, tschaefer@smail.uni-koeln.de,mbui@smail.uni-koeln.de,'
+                 'ppagejem@smail.uni-koeln.de',
     packages=find_namespace_packages(include=['*']),
     install_requires=['pandas', 'geopandas', 'shapely', 'numpy', 'scikit-learn', 'click', 'yaspin', 'joblib'],
     entry_points={
